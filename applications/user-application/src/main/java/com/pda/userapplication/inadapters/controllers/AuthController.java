2327dbf31a00e25186cb2b6e14d05525b9e40b89
Dongwon KIm <dongwon000103@gmail.com>
2024-06-11 10:44:02 +0900

    chore: test security api

--- applications/user-application/src/main/java/com/pda/userapplication/inadapters/controllers/AuthController.java
+++ applications/user-application/src/main/java/com/pda/userapplication/inadapters/controllers/AuthController.java
@@ -3,8 +3,8 @@ package com.pda.userapplication.inadapters.controllers;
 import com.pda.apiutils.ApiUtils;
 import com.pda.apiutils.GlobalResponse;
 import com.pda.tofinsecurity.jwt.JwtProvider;
-import com.pda.tofinsecurity.jwt.TokenableUser;
 import com.pda.tofinsecurity.user.AuthUser;
+import com.pda.tofinsecurity.user.AuthUserInfo;
 import com.pda.userapplication.inadapters.controllers.dto.req.SignInRequest;
 import com.pda.userapplication.inadapters.controllers.dto.req.SignUpRequest;
 import com.pda.userapplication.services.in.ReissueUseCase;
@@ -72,7 +72,7 @@ public class AuthController {
     @GetMapping("/test")
     @Operation(summary = "인증 & 인가 테스트", description = "인가 테스트를 위한 API입니다. Jwt 정보를 파싱해서 리턴합니다. (해당 API는 ADMIN 권한만 access 가능합니다.)",
         security = @SecurityRequirement(name = "bearerAuth"))
-    public GlobalResponse<TokenableUser> test(@AuthUser TokenableUser user) {
+    public GlobalResponse<AuthUserInfo> test(@AuthUser AuthUserInfo user) {
         return ApiUtils.success("인가 테스트 성공", user);
     }
 }
