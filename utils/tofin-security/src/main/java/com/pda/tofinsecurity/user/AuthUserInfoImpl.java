128124ff24df33190b8c87e6b8fe6bc7ca8c4938
Dongwon KIm <dongwon000103@gmail.com>
2024-06-12 11:10:35 +0900

    fix: auth user info add token info

--- utils/tofin-security/src/main/java/com/pda/tofinsecurity/user/AuthUserInfoImpl.java
+++ utils/tofin-security/src/main/java/com/pda/tofinsecurity/user/AuthUserInfoImpl.java
@@ -1,4 +1,4 @@
-package com.pda.tofinsecurity.jwt;
+package com.pda.tofinsecurity.user;
 
 import com.pda.tofinenums.user.Job;
 import com.pda.tofinenums.user.UserRole;
@@ -10,15 +10,16 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Builder
-public class TokenableUser implements TokenableInfo {
+public class AuthUserInfoImpl implements AuthUserInfo {
     private Long id;
-    private UserRole userRole;
-    private String profileImage;
     private String nickname;
+    private String profileImage;
+    private UserRole userRole;
     private LocalDate birth;
     private Job job;
+    private String token;
 }
