0e4968635389b9590a6114605a47d95436150ba3
bk <roboticts49@gmail.com>
2024-06-13 14:41:15 +0900

    docs: add swagger comment api Tag

--- applications/board-application/src/main/java/com/pda/boardapplication/controller/CommentController.java
+++ applications/board-application/src/main/java/com/pda/boardapplication/controller/CommentController.java
@@ -10,6 +10,7 @@ import com.pda.tofinsecurity.jwt.TokenableUser;
 import com.pda.tofinsecurity.user.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -23,6 +24,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/boards/{boardId}/comments")
 @Slf4j
+@Tag(name = "[Comment]")
 public class CommentController {
 
     private final CommentService commentService;
