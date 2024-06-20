fe5312ab885a6fbcd4342d3eff97eac352c67a4e
bk <roboticts49@gmail.com>
2024-06-20 11:25:31 +0900

    chore: add eureka client and actuator dependencies

--- applications/challenge-application/src/main/java/com/pda/challengeapplication/ChallengeApplication.java
+++ applications/challenge-application/src/main/java/com/pda/challengeapplication/ChallengeApplication.java
@@ -1,5 +1,8 @@
 package com.pda.challengeapplication;
 
+import com.netflix.discovery.EurekaClient;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +11,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication (scanBasePackages = {"com.pda"})
 public class ChallengeApplication {
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
     public static void main(String[] args) {
         SpringApplication.run(ChallengeApplication.class, args);
     }
 
+    @PreDestroy
+    public void unregister() {
+        System.out.println("Shutdown board");
+        eurekaClient.shutdown();
+    }
 }
