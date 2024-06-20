1ebf4fc2e3034ad2c8d8dcf81a0a047f66ab1e0d
Sieun Kim <se0983@kookmin.ac.kr>
2024-06-21 02:44:20 +0900

    fix: change category name to Korean

--- applications/product-application/src/main/resources/db/insert_001category.sql
+++ applications/product-application/src/main/resources/db/insert_001category.sql
@@ -1,5 +1,5 @@
 -- Category sql
-INSERT INTO category VALUES (1, "card");
-INSERT INTO category VALUES (2, "saving");
-INSERT INTO category VALUES (3, "fund");
-INSERT INTO category VALUES (4, "loan");
+INSERT INTO category VALUES (1, "카드");
+INSERT INTO category VALUES (2, "예적금");
+INSERT INTO category VALUES (3, "펀드");
+INSERT INTO category VALUES (4, "대출");
