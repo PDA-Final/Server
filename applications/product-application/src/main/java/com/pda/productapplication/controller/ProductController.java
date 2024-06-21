91ffc15b4855b8d1548b7e0495fee89a229f69aa
Sieun Kim <se0983@kookmin.ac.kr>
2024-06-21 13:29:47 +0900

    fix: modify tag name

--- applications/product-application/src/main/java/com/pda/productapplication/controller/ProductController.java
+++ applications/product-application/src/main/java/com/pda/productapplication/controller/ProductController.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Tag(name = "Product", description = "Product API")
+@Tag(name = "[Product]", description = "Product API")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
