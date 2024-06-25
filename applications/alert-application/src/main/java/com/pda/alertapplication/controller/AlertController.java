1fd440490108013aa7396caf3a728f5641e1d5f9
Sieun Kim <se0983@kookmin.ac.kr>
2024-06-25 10:48:36 +0900

    feat: add user security

--- applications/alert-application/src/main/java/com/pda/alertapplication/controller/AlertController.java
+++ applications/alert-application/src/main/java/com/pda/alertapplication/controller/AlertController.java
@@ -2,12 +2,17 @@ package com.pda.alertapplication.controller;
 
 import com.pda.alertapplication.service.AlertService;
 import com.pda.alertapplication.service.EmitterService;
+import com.pda.apiutils.ApiUtils;
+import com.pda.apiutils.GlobalResponse;
+import com.pda.tofinsecurity.user.AuthUser;
+import com.pda.tofinsecurity.user.AuthUserInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
@@ -19,17 +24,12 @@ public class AlertController {
     private final EmitterService emitterService;
     private final AlertService alertService;
 
-    @GetMapping(value = "/subscribe/{clientId}", produces = "text/event-stream")
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    @Operation(summary = "SSE Subscribe", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Success!")
     public SseEmitter subscribe(
-            @PathVariable Long clientId,
+            @AuthUser AuthUserInfo clientId,
             @PathVariable (value = "Last-Event-ID", required = false) String lastEventId) {
-        return emitterService.addEmitter(clientId, lastEventId);
+        return emitterService.addEmitter(clientId.getId(), lastEventId);
     }
-
-    // 알림 몇 개 안 읽었는지 알려주는
-    // 알림 페이지 열면 안 읽은 알림 0개됨
-
-    // 1. 구독
-    // 2. 카프카 오면 alert->client 로 전송: 메세지 온 거 다 보내고
-    // 3. clien는 특정 이벤트 발생하면 알림 서버에게 읽었따고 전송
 }
